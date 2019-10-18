795340b92e5025abca36b64a255e267a4fb273fa
hinault <ngdonfack@gmail.com>
2019-10-18 09:39:50 -0400

    Ajout Todo.cs

--- TodoAPI/TodoAPI/Data/TodoContext.cs
+++ TodoAPI/TodoAPI/Data/TodoContext.cs
@@ -3,10 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TodoAPI.Models;
 
 namespace TodoAPI.Data
 {
-    public class TodoContext
+    public class TodoContext : DbContext
     {
         public TodoContext(DbContextOptions<TodoContext> options)
            : base(options)
